953021de59cf31025127753c0d14676b71ec285d
donghun93 <alswn4516@gmail.com>
2023-03-16 19:00:17 +0900

    Feat: 게시글 조회 관련 어댑터 개발

--- src/main/java/com/devwinter/postservice/adapter/output/persistence/query/PostEntityQueryAdapter.java
+++ src/main/java/com/devwinter/postservice/adapter/output/persistence/query/PostEntityQueryAdapter.java
@@ -5,7 +5,9 @@ import com.devwinter.postservice.adapter.output.persistence.entity.PostEntity;
 import com.devwinter.postservice.application.port.output.LoadPostPort;
 import com.devwinter.postservice.common.PersistenceAdapter;
 import com.devwinter.postservice.common.exception.PostException;
+import com.devwinter.postservice.domain.MemberId;
 import com.devwinter.postservice.domain.Post;
+import com.devwinter.postservice.domain.PostId;
 import lombok.RequiredArgsConstructor;
 
 import static com.devwinter.postservice.common.exception.PostErrorCode.POST_NOT_FOUND;
@@ -15,12 +17,25 @@ import static com.devwinter.postservice.common.exception.PostErrorCode.POST_NOT_
 public class PostEntityQueryAdapter implements LoadPostPort {
 
     private final PostEntityQueryRepository postEntityQueryRepository;
-    private final PostMapper postMapper;
 
     @Override
     public Post load(Long postId) {
         PostEntity postEntity = postEntityQueryRepository.findByPostId(postId)
                                                          .orElseThrow(() -> new PostException(POST_NOT_FOUND));
-        return postMapper.entityToDomain(postEntity);
+        return entityToDomain(postEntity);
+    }
+
+    private Post entityToDomain(PostEntity postEntity) {
+        return Post.builder()
+                   .id(new PostId(postEntity.getId()))
+                   .memberId(new MemberId(postEntity.getMemberId()))
+                   .title(postEntity.getTitle())
+                   .contents(postEntity.getContents())
+                   .category(postEntity.getCategory())
+                   .deleted(postEntity.isDeleted())
+                   .createdAt(postEntity.getCreatedAt())
+                   .modifiedAt(postEntity.getModifiedAt())
+                   .deletedAt(postEntity.getDeletedAt())
+                   .build();
     }
 }
